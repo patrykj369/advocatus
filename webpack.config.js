49a2919771be3da02486791d0a760f970cff0684
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2021-07-20 21:01:15 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  resolve: {
+    extensions: [".js", ".json"]
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new CopyPlugin([
+      {
+        from: "src/*.html",
+        to: "",
+        flatten: true
+      }
+    ])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          {
+            loader: "style-loader"
+            // options: {
+            //   // injectType: "singletonStyleTag"
+            //   // injectType: "linkTag"
+            // }
+          },
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
+      }
+    ]
+  }
+};
