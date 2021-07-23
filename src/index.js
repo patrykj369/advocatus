e70d4719c70f72f49b52047f3b38084c14cb5ef1
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2021-07-23 22:44:39 +0200

    try add slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './main.scss';
+
+const x = document.querySelector(".next");
+const z = document.querySelector(".prev");
+let current = 1;
+x.addEventListener("click", () => plusSlides());
+z.addEventListener("click", () => plusSlides());
+
+const plusSlides = () => {
+    const container = document.querySelectorAll(".name-surname-button-container");
+
+
+    console.log(container);
+    for(let i = 0; i < container.length; i++){
+        if(container[i].id === "photo-slider-1"){
+            container[i]
+        }
+    }
+
+
+}
